21a6138beb2e90ece293b99615d059c567517bea
Ghassen <latrechghassen68@gmail.com>
2023-09-12 17:21:02 +0100

    fixing the update link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -37,7 +37,9 @@ cart.forEach((cartItem) => {
           <span>
             Quantity: <span class="quantity-label">${cartItem.quantity}</span>
           </span>
-          <span class="update-quantity-link link-primary">
+          <input class="quantity-input">
+          <span class="save-quantity-link link-primary">Save</span>
+          <span class="update-quantity-link link-primary js-update-quantity-link data-product-id="${matchingProduct.id}">
             Update
           </span>
           <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
@@ -114,5 +116,14 @@ function updateCartQuantity(){
 
 updateCartQuantity();
 
+
+document.querySelectorAll('.js-update-quantity-link')
+  .forEach((link) => {
+  link.addEventListener('click' , () => {
+      const productId = link.dataset.productId;
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.classList.add('is-editing-quantity');
+  });
+}); 
 
 
